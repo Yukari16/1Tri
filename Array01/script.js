472976ac9ebd9a66bee678aebdab65200e8773b9
Yukari16 <yukari.nicole.nunes@gmail.com>
2024-10-17 11:27:02 -0300

    Array01

---
+++ Array01/script.js
@@ -0,0 +1,14 @@
+// VARIAVEIES UNITARIAS
+const nome = 'Jonas'
+const nome2 = 'Julia'
+const nome3 = 'Marcos'
+//...
+
+// VARIAVEIS DE ARRAY
+const nomes = ['Jonas', 'Julia', 'Marcos']
+
+// IMPRIMINDO OS VALORES DO ARRY
+console.log(nomes)
+console.log(nomes[2])
+nomes[10] = 'Dino' // CRIANDO DINAMICAMENTE
+console.log(nomes[10])
