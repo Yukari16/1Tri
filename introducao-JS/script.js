8fa9f2c7d96f8859b0b8b556912ff20dfc2bc9ee
Yukari16 <yukari.nicole.nunes@gmail.com>
2024-10-17 11:21:16 -0300

    Introdução JS

---
+++ introducao-JS/script.js
@@ -0,0 +1,14 @@
+const idade =17
+// crie uma variavel para cada um dos
+// dados listados (nome, sobrenome, telefone, estacasado(true ou false))
+// imprima cada um dos valores com console log<variavel>)
+
+console.log(idade)
+
+const sobrenome = "Souza"
+const telefone = "41 6669-6069"
+const estacasado = true
+
+console.log(sobrenome)
+console.log(telefone)
+console.log(true)
